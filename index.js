3d81fb4c77207df26d1bb756f7ded54c2399faac
Yutaka Houlette <yutakahoulette@gmail.com>
2019-01-27 18:09:54 -0800

    Update content

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ import snabbdom from 'snabbdom'
 import h from 'snabbdom/h'
 import images from './js/images'
 import header from './js/header'
-import code from './js/code'
 import fadeIn from './js/fade-in'
 import brickIt from './js/brick-it'
 import link from './js/link'
 
-const data = {modalData: {}}
+const data = { modalData: {} }
 
 const patch = snabbdom.init([
   require('snabbdom/modules/class'),
@@ -20,11 +19,8 @@ const patch = snabbdom.init([
 
 const isNarrow = window.innerWidth <= 400
 
-const view = data => {
-  let content = [
-    header(),
-    main()
-  ]
+const view = (data) => {
+  let content = [header(), main()]
   content = data.modalData.src
     ? R.concat(content, imageModal(data.modalData))
     : content
@@ -35,92 +31,94 @@ const footer = () =>
   h('footer.px-1.py-3.mt-3', [
     h('hr'),
     h('ul.mt-3.list-reset', [
+      h('li', [link('', 'https://github.com/yutakahoulette', 'Github', true)]),
       h('li', [
-        link('', 'https://github.com/yutakahoulette', 'Github', true)
+        link(
+          '',
+          'https://www.linkedin.com/in/yutakahoulette/',
+          'LinkedIn',
+          true
+        )
       ]),
-      h('li', [
-        link('', 'https://www.linkedin.com/in/yutakahoulette/', 'LinkedIn', true)
-      ]),
-      h('li', [
-        h('a', {props: {href: 'mailto:yutakahoulette@gmail.com'}}, 'yutakahoulette@gmail.com')
-      ])
+      h('li', ['yutakahoulette[at]gmail.com'])
     ]),
-    h('p.m-0.small', 'All images copyright © 2018 Yutaka Houlette')
+    h(
+      'p.m-0.small',
+      `All images copyright ©  ${new Date().getFullYear()} Yutaka Houlette`
+    )
   ])
 
-const main = _ =>
-  h('main', [
-    h('div.p-05.mb-2', [ h('hr') ]),
-    code(),
-    h('div.p-05.mb-2', [ h('hr') ]),
-    illustration(),
-    footer(),
-    largeImages()
-  ]
-  )
+const main = (_) =>
+  h('main', [h('div.p-05', [h('hr')]), illustration(), footer(), largeImages()])
 
-const illustration = _ =>
+const illustration = (_) =>
   h('section.p-05', [
-    h('div.p-05', [
-      h('h3.m-0', 'Illustration projects')
-    ]),
     h('div.pt-2.pb-4', [
       h('h4.p-05.mb-2', 'Fred Korematsu Speak Up'),
-      h('div', R.map(x => imgBox(x, '.col-4'), images.korematsu))
+      h('div', R.map((x) => imgBox(x, '.col-4'), images.korematsu))
     ]),
     h('h4.p-05.mb-2', 'Editorial and personal work'),
-    h('div.transO', {
-      hook: {insert: isNarrow ? _ => '' : brickIt},
-      class: {'opacity-0': !isNarrow}}
-      , R.map(x => imgBox(x), images.illo)
+    h(
+      'div.transO',
+      {
+        hook: { insert: isNarrow ? (_) => '' : brickIt },
+        class: { 'opacity-0': !isNarrow }
+      },
+      R.map((x) => imgBox(x), images.illo)
     )
   ])
 
 const imgBox = (o, className) =>
   h(`div.inline-block.relative.${className || ''}`, [
-    h('figure.m-0.relative.opacity-0.transO'
-    , {
-      on: {click: openModal},
-      class: {'cursor-pointer': !isNarrow},
-      hook: {insert: fadeIn}
-    }
-    , [
-      h('img.p-05.width-full', {
-        props: { src: `images/${o.src}.jpg`, alt: o.title },
-        attrs: {'data-large-image': `images/${o.large}.jpg`}
-      }),
-      h('figcaption.absolute.p-1.bg-scrim-2.opacity-0.transO.line-height-2', o.title)
-    ])
+    h(
+      'figure.m-0.relative.opacity-0.transO',
+      {
+        on: { click: openModal },
+        class: { 'cursor-pointer': !isNarrow },
+        hook: { insert: fadeIn }
+      },
+      [
+        h('img.p-05.width-full', {
+          props: { src: `images/${o.src}.jpg`, alt: o.title },
+          attrs: { 'data-large-image': `images/${o.large}.jpg` }
+        }),
+        h(
+          'figcaption.absolute.p-1.bg-scrim-2.opacity-0.transO.line-height-2',
+          o.title
+        )
+      ]
+    )
   ])
 
-const hiddenImg = o => h('img.hide', {props: {src: `images/${o.large}.jpg`}})
+const hiddenImg = (o) =>
+  h('img.hide', { props: { src: `images/${o.large}.jpg` } })
 
-const largeImages = _ =>
+const largeImages = (_) =>
   h('div', [
     h('div', R.map(hiddenImg, images.korematsu)),
     h('div', R.map(hiddenImg, images.illo))
   ])
 
 const imageModal = (modalData) =>
-  h('div.fixed.bottom-0.right-0.top-0.left-0.bg-scrim-2.opacity-0.transO',
+  h(
+    'div.fixed.bottom-0.right-0.top-0.left-0.bg-scrim-2.opacity-0.transO',
     {
       style: {
-        delayed: {opacity: '1'},
-        remove: {opacity: '0'}
+        delayed: { opacity: '1' },
+        remove: { opacity: '0' }
       },
-      on: {click: closeModal}
-    }, [
+      on: { click: closeModal }
+    },
+    [
       h('div.width-full.height-full.center.p-2', [
-        h('img.shadow-4.verticallyCenter.fullMaxHeight',
-          {
-            props: {src: modalData.src, alt: modalData.alt}
-          }
-        )
+        h('img.shadow-4.verticallyCenter.fullMaxHeight', {
+          props: { src: modalData.src, alt: modalData.alt }
+        })
       ])
     ]
   )
 
-const openModal = e => {
+const openModal = (e) => {
   if (isNarrow) return
   let image = e.target.parentElement.querySelector('img')
   data.modalData.src = image.getAttribute('data-large-image')
@@ -128,7 +126,7 @@ const openModal = e => {
   render()
 }
 
-const closeModal = e => {
+const closeModal = (e) => {
   if (e.target.tagName.toLocaleLowerCase() === 'img') return
   data.modalData.src = false
   render()
@@ -136,7 +134,7 @@ const closeModal = e => {
 
 let vnode
 
-const render = _ => {
+const render = (_) => {
   vnode = patch(vnode, view(data))
 }
 
--- js/header.js
+++ js/header.js
@@ -1,19 +1,32 @@
 import link from './link'
 import h from 'snabbdom/h'
 
-module.exports = _ =>
-  h('header.p-1.mb-2', [
-    h('h1.h2.mb-1', 'Yutaka Houlette'),
+const a = (text, url) => link('', url, text, true)
+
+module.exports = (_) =>
+  h('header.p-1.mb-2.mt-4', [
+    h('h1.h3.mb-1', 'Yutaka Houlette'),
     h('article.pt-2.readable', [
-      h('p.bump', "Hello! I'm a web developer and occasional illustrator based in Oakland, California."),
+      h(
+        'p.bump',
+        "I'm a web developer and occasional illustrator based in Oakland, California."
+      ),
       h('p.bump', [
-        "I'm currently working at Media Cause",
-        ",  where I'm helping to build an activism tool called ",
-        link('', 'https://rallystarter.com', 'Rally Starter', true),
-        '. ',
         'My most recent illustration project is a book about the Japanese-American civil rights hero Fred Korematsu, titled ',
-        link('', 'https://www.amazon.com/Fred-Korematsu-Speaks-Fighting-Justice/dp/1597143685', "'Fred Korematsu Speaks Up'", true),
-        '.'
+        a(
+          "'Fred Korematsu Speaks Up'",
+          'https://www.amazon.com/Fred-Korematsu-Speaks-Fighting-Justice/dp/1597143685'
+        ),
+        '. ',
+        'I enjoy building ',
+        a('design systems', 'https://rallystarter.com/design-system'),
+        ' and using code to ',
+        a(
+          'make interactive art',
+          'https://yutakahoulette.gitlab.io/sketchbook/?sketch=4'
+        ),
+        '. ',
+        "I'm currently working at Dropbox as a frontend engineer."
       ])
     ])
   ])
